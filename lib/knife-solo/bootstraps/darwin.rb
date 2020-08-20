1b8d6fededfe9d3543d765d5b05ab2bdaa1b56ed
Andrew Halligan <andrew.halligan@uken.com>
2020-08-20 16:04:13 -0400

    Support for Catalina (10.15.X)

--- lib/knife-solo/bootstraps/darwin.rb
+++ lib/knife-solo/bootstraps/darwin.rb
@@ -7,7 +7,7 @@ module KnifeSolo::Bootstraps
 
     def distro
       case issue
-      when %r{10.(?:[6-9]|10|11|12|13|14)}
+      when %r{10.(?:[6-9]|10|11|12|13|14|15)}
         {:type => 'omnibus'}
       else
         raise "OS X version #{issue} not supported"
